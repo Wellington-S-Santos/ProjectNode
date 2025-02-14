232f57260db75314dff494e703d34fde042b8b4e
Wellington-S-Santos <wellington234.az@gmail.com>
2025-02-13 22:26:54 -0300

    First Commit

---
+++ Aula012025/index.js
@@ -0,0 +1,63 @@
+const express = require ("express");
+//const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res) => {
+    res.send('Hello Word!!');
+})
+
+    app.get('/calculadora',(req,res) =>{
+        const { num1,num2, operacao} = req.query;
+
+        //Conveter parâmetros para calcular
+        const number1 = parseFloat (num1);
+        const number2 = parseFloat (num2);
+
+        //verifica se os parâmetros são números válidos
+        if(isNaN(number1) || isNaN(number2)){
+            //throw new Error ('parâmetros inválidos!');
+            return res.status(400).json({ erro : "numero1 e numero2 devem ser convertidos corretamente"});
+        }
+
+        let result;
+
+        switch (operacao) {
+            case 'soma':
+                result = number1 + number2;                
+                break;
+
+            case 'subtracao':
+                result = number1 - number2;
+                break;
+            
+            case 'multiplicacao':
+                result = number1 * number2;
+                break;
+            case 'divisao':
+                if (number2 === 0) {
+                    return res.status(400).json({erro: "Divisão por zero não é permitido !!"});
+                }
+                result = number1 / number2;
+                break;
+        
+            default:
+                return res.status(400).json({erro: "Operação inválida!!"});
+                
+        }
+        res.json({result});
+
+});
+
+ app.listen(port,()=>{
+    console.log(`Servidor rodando na porta ${port}`);
+ });
+ 
+ process.on('uncaughtException', (err)=> {
+    console.error("Erro não tratado:" , err);
+ });
+
+ process.on("unhandledRejection", (err) =>{
+    console.error("Regeição não tratada:", err);
+ });
